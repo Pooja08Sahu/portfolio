2b93d833ca133110d2e6baf5d00f6fa4fa5bd831
Pooja08Sahu <ps9387250@gmail.com>
2026-06-12 17:49:31 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,196 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta charset="UTF-8">
+  <title>Pooja Sahu | Portfolio</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<header>
+  <div class="logo">Pooja Sahu</div>
+  <nav>
+    <a href="#home">Home</a>
+    <a href="#about">About</a>
+    <a href="#skills">Skills</a>
+    <a href="#projects">Projects</a>
+    <a href="#contact">Contact</a>
+  </nav>
+</header>
+
+<!-- HERO -->
+<section id="home" class="hero">
+  <div class="hero-box">
+    <img src="image/pooja.jpeg" class="profile-img">
+    <h1>Hi, I'm <span>Pooja Sahu</span></h1>
+    <h2 class="typing"></h2>
+    <p>Diploma CSE Student | Frontend Developer</p>
+
+    <div class="btn-group">
+      <a href="image/pujasahu..pdf" download class="btn">Download Resume</a>
+      <a href="#contact" class="btn-outline">Hire Me</a>
+    </div>
+  </div>
+</section>
+
+<!-- ABOUT -->
+<section id="about">
+  <h2 class="edu-title">About Me</h2>
+  <p>
+    I am a Diploma Computer Science Engineering student.  
+    I love building modern websites and learning new technologies.  
+    I am passionate about Frontend Development and UI Design.
+  </p>
+</section>
+
+
+<!-- Education -->
+<section id="education">
+  <h2 class="edu-title">🎓 Education</h2>
+
+  <div class="edu-card">
+    <h3>2023 - 2026</h3>
+    <p><b>Diploma in CSE</b></p>
+    <span>Government Girls Polytechnic Meja, Prayagraj</span>
+  </div>
+
+  <div class="edu-card">
+    <h3>2022</h3>
+    <p><b>High School</b></p>
+    <span>B.P.O. Intermediate College</span>
+    <ul>
+      Complete Education
+      Focused on academic subjects and Fundamentals.
+    </ul>
+  </div>
+
+</section>
+
+<!-- SKILLS -->
+<section id="skills">
+  <h2 class="edu-title">My Skills</h2>
+  <div class="skills">
+    <div>HTML</div>
+    <div>CSS</div>
+    <div>JavaScript</div>
+    <div>BootStrap</div>
+    <div>GitHub</div>
+    <div>React (Learning)</div>
+  </div>
+</section>
+
+<!-- PROJECTS -->
+<section id="projects">
+  <h2 class="edu-title">My Projects</h2>
+
+  <div class="projects">
+
+    <div class="project-card">
+      <h3>Digital Clock</h3>
+      <p>Real-time digital clock using JavaScript.</p>
+      <div class="project-btns">
+        <a href="#" class="btn">Live Demo</a>
+        <a href="#" class="btn-outline">GitHub</a>
+      </div>
+    </div>
+
+    <div class="project-card">
+      <h3>Weather App</h3>
+      <p>Real-time weather app with API integration using HTML, CSS, JavaScript.</p>
+      <div class="project-btns">
+        <a href="#" class="btn">Live Demo</a>
+        <a href="https://github.com/Pooja08Sahu/weather" class="btn-outline" target="_blank">GitHub</a>
+      </div>
+    </div>
+
+<div class="project-card">
+      <h3>Portfolio Website</h3>
+      <p>Personal responsive portfolio website.</p>
+      <div class="project-btns">
+        <a href="#" class="btn">Live Demo</a>
+        <a href="#" class="btn-outline">GitHub</a>
+      </div>
+    </div>
+
+    <div class="project-card">
+      <h3>Portfolio Website</h3>
+      <p>Personal responsive portfolio website.</p>
+      <div class="project-btns">
+        <a href="#" class="btn">Live Demo</a>
+        <a href="#" class="btn-outline">GitHub</a>
+      </div>
+    </div>
+
+    <div class="project-card">
+      <h3>Portfolio Website</h3>
+      <p>Personal responsive portfolio website.</p>
+      <div class="project-btns">
+        <a href="#" class="btn">Live Demo</a>
+        <a href="#" class="btn-outline">GitHub</a>
+      </div>
+    </div>
+
+    <div class="project-card">
+      <h3>Portfolio Website</h3>
+      <p>Personal responsive portfolio website.</p>
+      <div class="project-btns">
+        <a href="#" class="btn">Live Demo</a>
+        <a href="#" class="btn-outline">GitHub</a>
+      </div>
+    </div>
+
+    <div class="project-card">
+      <h3>Portfolio Website</h3>
+      <p>Personal responsive portfolio website.</p>
+      <div class="project-btns">
+        <a href="#" class="btn">Live Demo</a>
+        <a href="#" class="btn-outline">GitHub</a>
+      </div>
+    </div>
+ 
+
+  </div>
+</section>
+
+
+<!--Any query-->
+<div class="form-container">
+    <h2>Any query</h2>
+    
+    <form action="form.php" method="post">
+        <input type="text" placeholder="Your Name" name="name" required>
+        <input type="email" placeholder="Your Email" name="email" required>
+        <input type="tel" placeholder="Phone Number" name="mobile" required>
+        <textarea placeholder="Any Query..." name="message" required></textarea>
+        <button type="submit">Send Message</button>
+    </form>
+
+</div>
+
+
+
+<!-- CONTACT -->
+<section id="contact">
+  <h2 class="edu-title">Contact Me</h2>
+         <a>Email: ps9387250@gmail.com</a>
+    <div class="phone">
+        <a>Phone: 9170981928</a>
+</section>
+<div>   </div>
+
+<!--socail-->
+<div class="socail">
+    <a href="https://www.linkedin.com/in/pooja-sahu-96a747358?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" target="_blank"><img src="image/linkin1.png"></a>
+    <a href="https://www.instagram.com/pooja_34162?igsh=MWx1dXhiMDE0dWxpNA==" target="_blank"><img src="image/instagram.svg"></a>
+    <a href="https://github.com/Pooja08Sahu" target="_blank"><img src="image/github.svg"></a>
+</div>
+
+
+<footer>
+  <p>© 2026 Pooja Sahu | Frontend Developer</p>
+</footer>
+
+<script src="javascript.js"></script>
+</body>
+</html>
